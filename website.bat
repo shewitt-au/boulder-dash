7c7498efa39699d1d2d035e38a2cf0b0834ab32c
shewitt.au <shewitt.au@gmail.com>
2018-07-09 18:45:59 +1000

    Modified the website script to open an explorer window when done

--- website.bat
+++ website.bat
@@ -1,1 +1,2 @@
 ..\diss\diss.py -p prefix.txt -c MemType.txt -c C64.txt -t "Boulder Dash Disassembly" ..\..\GitHub\shewitt-au.github.io\boulder-dash\boulder-dash-disassembly.html
+explorer ..\..\GitHub\shewitt-au.github.io\boulder-dash
