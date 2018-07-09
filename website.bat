0886f25668b872be5314fa50f7aa7c61c7ac1262
shewitt.au <shewitt.au@gmail.com>
2018-07-09 15:01:59 +1000

    Correction

--- website.bat
+++ website.bat
@@ -1,1 +1,1 @@
-..\diss\diss.py -p prefix -c MemType.txt -c C64.txt -t "Boulder Dash Disassembly" ..\..\GitHub\shewitt-au.github.io\boulder-dash\boulder-dash-disassembly.html
+..\diss\diss.py -p prefix.txt -c MemType.txt -c C64.txt -t "Boulder Dash Disassembly" ..\..\GitHub\shewitt-au.github.io\boulder-dash\boulder-dash-disassembly.html
