96dde77e34b39e046e370756170d84c32d2a5eeb
shewitt.au <shewitt.au@gmail.com>
2018-07-27 23:02:43 +1000

    Update scripts to reflect new tool name

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-..\diss\diss.py -c MemType.txt -c C64.txt -t "Boulder Dash Disassembly" site\boulder-dash-disassembly.html
+..\negentropy\negentropy.py -c MemType.txt -c C64.txt -t "Boulder Dash Disassembly" site\boulder-dash-disassembly.html
