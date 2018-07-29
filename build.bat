62b4295dda056497b867789540ab6b709e829018
shewitt.au <shewitt.au@gmail.com>
2018-07-29 20:09:04 +1000

    C64 symbols are now part of Negentropy

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-..\negentropy\negentropy.py -c MemType.txt -c C64.txt -t "Boulder Dash Disassembly" site\boulder-dash-disassembly.html
+..\negentropy\negentropy.py -c MemType.txt -t "Boulder Dash Disassembly" site\boulder-dash-disassembly.html
--- website.bat
+++ website.bat
@@ -1,2 +1,2 @@
-..\negentropy\negentropy.py -p prefix.txt -c MemType.txt -c C64.txt -t "Boulder Dash Disassembly" ..\..\GitHub\shewitt-au.github.io\boulder-dash\boulder-dash-disassembly.html
+..\negentropy\negentropy.py -p prefix.txt -c MemType.txt -t "Boulder Dash Disassembly" ..\..\GitHub\shewitt-au.github.io\boulder-dash\boulder-dash-disassembly.html
 explorer ..\..\GitHub\shewitt-au.github.io\boulder-dash
