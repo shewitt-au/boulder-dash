6248d7f8f816a9f619c181be20294a6ac6100c22
shewitt.au <shewitt.au@gmail.com>
2018-07-30 12:14:51 +1000

    Use new builton option

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-..\negentropy\negentropy.py -c MemType.txt -t "Boulder Dash Disassembly" site\boulder-dash-disassembly.html
+..\negentropy\negentropy.py -c MemType.txt -t "Boulder Dash Disassembly" -b site\boulder-dash-disassembly.html
--- website.bat
+++ website.bat
@@ -1,2 +1,2 @@
-..\negentropy\negentropy.py -p prefix.txt -c MemType.txt -t "Boulder Dash Disassembly" ..\..\GitHub\shewitt-au.github.io\boulder-dash\boulder-dash-disassembly.html
+..\negentropy\negentropy.py -p prefix.txt -c MemType.txt -t "Boulder Dash Disassembly" -b ..\..\GitHub\shewitt-au.github.io\boulder-dash\boulder-dash-disassembly.html
 explorer ..\..\GitHub\shewitt-au.github.io\boulder-dash
