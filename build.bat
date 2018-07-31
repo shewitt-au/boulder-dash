29b9bb02d8fe62e373432194903fe5af9d401b3b
shewitt.au <shewitt.au@gmail.com>
2018-08-01 00:27:16 +1000

    Use new command (now Negentropy is a package)

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-..\negentropy\negentropy.py -c MemType.txt -t "Boulder Dash Disassembly" -b site\boulder-dash-disassembly.html
+negentropy -c MemType.txt -t "Boulder Dash Disassembly" -b site\boulder-dash-disassembly.html
